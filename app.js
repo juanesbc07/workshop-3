d5b3467828d033c356e31e1ca84c5f060d5481cb
juanesbc07 <juanestebanbarrero@hotmail.com>
2020-12-09 15:38:11 -0500

    initial setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Workshop-3 project run on listening at http://localhost:${port}`);
+});
